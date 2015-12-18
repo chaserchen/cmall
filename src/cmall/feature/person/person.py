e60114e777ae3af195915ee1d7b9018d588f25c0
Guo Chen <chenguo@honovation.com>
2015-12-18 23:03:19 +0800

    starter

---
+++ src/cmall/feature/person/person.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals, print_function, division
+import logging
+from veil.profile.web import *
+from veil.profile.model import *
+from cmall.const import GENDERS
+
+LOGGER = logging.getLogger(__name__)
+db = register_database('cmall')
+
+
+def list_persons():
+    return db().list('SELECT * FROM person ORDER BY id')
+
+
+@template_filter('gender')
+def render_gender(value):
+    return GENDERS[value]
---
+++ src/cmall/website/person/person.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals, print_function, division
+from veil.profile.web import *
+from cmall.feature.person import *
+
+person_route = route_for('person')
+
+
+@person_route('GET', '/')
+def list_persons_page():
+    return get_template('list-persons.html').render(persons=list_persons())
